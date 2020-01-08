31bcb0aa231fa8ef9e1cd27696835fdfcb709271
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-01-08 15:20:36 +0530

    new commit

--- first_Form.php
+++ first_Form.php
@@ -9,7 +9,6 @@
 		$message="ProjectName:"."Prestige Waterford"."\n"."Name :".$name."\n"."Phone :".$phone."\n"."Email :".$email."\n";
 		$headers="From: ".$email;
 		if(mail($to, $subject, $message, $headers)){
-		
 		echo "<h1  style=\"text-align:center\">Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
 		// echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1cQjo-FwWNmCqFv_71lLac2heQyOIGTZT">Click here to download Brochure [Hyde]</a><h3>';
 		// echo '<h3 style="text-align:center"><a href="https://drive.google.com/open?id=1uCIKnYTyTH27_ueX1dvAIbAG_UCtOcKd">Click here to download Brochure [Regent]</a><h3>';
